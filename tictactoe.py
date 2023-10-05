1badb59ce767481fc210daa04c4085b353270ab8
begfam09 <146381780+begfam09@users.noreply.github.com>
2023-10-05 17:05:10 +0000

    Implemented Functions

--- tictactoe.py
+++ tictactoe.py
@@ -1,8 +1,8 @@
 """
 Tic Tac Toe Player
 """
-
 import math
+import copy
 
 X = "X"
 O = "O"
@@ -22,46 +22,149 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    count_x = 0
+    count_o = 0
+
+    for row in board:
+        for cell in row:
+            if cell == 'X':
+                count_x += 1
+            elif cell == 'O':
+                count_o += 1
+
+    if count_x > count_o:
+        return O
+
+    return X
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    set_actions = set()
+
+    for r_index, row in enumerate(board):
+        for c_index, col in enumerate(row):
+            if col is EMPTY:
+                set_actions.add((r_index, c_index))
+    
+    return set_actions
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+
+    new_board = copy.deepcopy(board)
+
+    row = action[0]
+    col = action[1]
+
+    new_board[row][col] = player(board)
+    return new_board
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    candidates = []
+    candidates.append([board[0][0], board[0][1], board[0][2]])
+    candidates.append([board[1][0], board[1][1], board[1][2]])
+    candidates.append([board[2][0], board[2][1], board[2][2]])
+    candidates.append([board[0][0], board[1][0], board[2][0]])
+    candidates.append([board[0][1], board[1][1], board[2][1]])
+    candidates.append([board[0][2], board[1][2], board[2][2]])
+    candidates.append([board[0][0], board[1][1], board[2][2]])
+    candidates.append([board[2][0], board[1][1], board[0][2]])
+
+    for candidate in candidates:
+        if candidate[0] == candidate[1] == candidate[2]:
+            if candidate[0] is not EMPTY:
+                return candidate[0]
+    
+    return None
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    if winner(board) in (X, O):
+        return True
+
+    for row in board:
+        for col in row:
+            if col is EMPTY:
+                return False
+
+    return True
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    if winner(board) == X:
+        return 1
+    if winner(board) == O:
+        return -1
+    
+    return 0
 
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    if terminal(board):
+        return None
+    
+    action_set = actions(board)
+
+    if player(board) == X:
+        v = -math.inf
+        for action in action_set:
+            result_value = minvalue(result(board, action), v)
+            if result_value > v:
+                v = result_value
+                best_action = action
+    
+    else:
+        v = math.inf
+        for action in action_set:
+            result_value = maxvalue(result(board, action), v)
+            if result_value < v:
+                v = result_value
+                best_action = action
+    
+    return best_action
+
+def maxvalue(board, best_value):
+    v = -math.inf
+
+    if terminal(board):
+        return utility(board)
+    
+    for action in actions(board):
+        v = max(v, minvalue(result(board, action), v))
+        if v > best_value:
+          return v
+    
+    return v
+
+
+def minvalue(board, best_value):
+    v = math.inf
+
+    if terminal(board):
+        return utility(board)
+    
+    for action in actions(board):
+        v = min(v, maxvalue(result(board, action), v))
+        if v < best_value:
+          return v
+
+    return v
